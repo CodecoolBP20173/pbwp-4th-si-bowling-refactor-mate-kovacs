617178ce91e8b28e5df328c3752c1de4221bc5a6
Kovács Máté <kismate90@gmail.com>
2017-11-22 15:40:10 +0100

    obvious clutter removed

--- bowling.py
+++ bowling.py
@@ -4,31 +4,28 @@ def score(game):
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            result += 10 - get_value(game[i-1])
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                result += get_value(game[i + 1])
+                if game[i + 2] == '/':
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
-        if in_first_half == True:
+                    result += get_value(game[i + 2])
+        if in_first_half is True:
             in_first_half = False
         else:
+            frame += 1
             in_first_half = True
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
+
 
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
