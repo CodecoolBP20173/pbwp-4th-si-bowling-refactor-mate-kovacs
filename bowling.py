a5ae2d22ee3972eca8548a7f73e9e8655468b2b3
Kovács Máté <kismate90@gmail.com>
2017-11-23 16:35:45 +0100

    is_normal_hit, is_spare, is_miss functions added and the conditional checks throughout the code are changed to use these. MAX_FRAME also changed to GAME_LENGTH - probably better name

--- bowling.py
+++ bowling.py
@@ -1,11 +1,11 @@
 def score(game):
-    MAX_FRAME = 10
+    GAME_LENGTH = 10
     result = 0
     frame = 1
     first_roll_in_round = True
     for current_roll in range(len(game)):
         result += get_value(game[current_roll])
-        result = multi_roll_corrections(frame, game, current_roll, result, MAX_FRAME)
+        result = multi_roll_corrections(frame, game, current_roll, result, GAME_LENGTH)
 
         if first_roll_in_round:
             first_roll_in_round = False
@@ -20,11 +20,11 @@ def score(game):
 
 def multi_roll_corrections(frame, game, current_roll, result, game_length):
     if frame < game_length and get_value(game[current_roll]) == 10:
-        if game[current_roll] == '/':
+        if is_spare(game[current_roll]):
             result += get_value(game[current_roll + 1]) - get_value(game[current_roll - 1])
         elif is_strike(game[current_roll]):
             result += get_value(game[current_roll + 1])
-            if game[current_roll + 2] == '/':
+            if is_spare(game[current_roll + 2]):
                 result += get_value(game[current_roll]) - get_value(game[current_roll + 1])
             else:
                 result += get_value(game[current_roll + 2])
@@ -32,22 +32,41 @@ def multi_roll_corrections(frame, game, current_roll, result, game_length):
 
 
 def get_value(roll_value):
-    if roll_value == '1' or roll_value == '2' or roll_value == '3' or \
-       roll_value == '4' or roll_value == '5' or roll_value == '6' or \
-       roll_value == '7' or roll_value == '8' or roll_value == '9':
+    if is_normal_hit(roll_value):
         return int(roll_value)
     elif is_strike(roll_value):
         return 10
-    elif roll_value == '/':
+    elif is_spare(roll_value):
         return 10
-    elif roll_value == '-':
+    elif is_miss(roll_value):
         return 0
     else:
         raise ValueError()
 
 
+def is_normal_hit(roll_value_sign):
+    if roll_value_sign.isdigit():
+        return True
+    else:
+        return False
+
+
+def is_spare(roll_value_sign):
+    if roll_value_sign == '/':
+        return True
+    else:
+        return False
+
+
 def is_strike(roll_value_sign):
     if roll_value_sign.lower() == 'x':
+        return True
+    else:
+        return False
+
+
+def is_miss(roll_value_sign):
+    if roll_value_sign == '-':
         return True
     else:
         return False
